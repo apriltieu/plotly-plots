1349b70f9b14eec70d9fb2691054a3b9b2faf456
April <aprilmtieu@gmail.com>
2022-12-19 11:17:46 +1100

    callback function to show and unshow divs for single run and all runs

--- vismetrics/metrics-dashboard.py
+++ vismetrics/metrics-dashboard.py
@@ -1,30 +1,77 @@
-# importing Pandas libary
 import pandas as pd
 from dash import Dash, dcc, Output, Input, html  # pip install dash
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 
-# Build your components
+# -------------------------------
+# Building components
+# -------------------------------
 app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
-myinput = dcc.Markdown(children="# Simulation results")
+title = dcc.Markdown(children="# Simulation results")
 
 # Create switches
-switches = html.Div(
+run_switch = html.Div(
     [
+        html.P(
+            "All runs",
+            style={"display": "inline-block"}
+        ),
         dbc.Checklist(
             options=[
-                {"label": "Single run", "value": 1},
+                {"label": "Single run", "value": "single"},
             ],
-            value=[1],
-            id="switches-input",
+            value=["single"],
+            id="run-switch",
             switch=True,
+            style={"display": "inline-block", "margin-left": "10px"}
         ),
+
     ]
 )
-# Customize your own Layout
-app.layout = dbc.Container(
-    [myinput,switches]
+
+# All runs div
+all_runs = html.Div(
+    [
+        html.P("ALL RUNS")
+    ],
+    id="all-runs-components",
+    style={"display": "block"} # This line is changed by single_or_all function
 )
+
+# Single run div
+single_run = html.Div(
+    [
+        html.P("Hello this is for a single run")
+    ],
+    id="single-run-components",
+    style={"display": "none"} # This line is changed by single_or_all function
+)
+
+# -------------------------------
+# Customise layout
+# -------------------------------
+app.layout = dbc.Container(
+    [title, run_switch, single_run, all_runs]
+)
+
+# -------------------------------
+# Call back functions
+# -------------------------------
+@app.callback(
+    [
+        Output(component_id="single-run-components", component_property="style"),
+        Output(component_id="all-runs-components", component_property="style")
+    ],
+    Input(component_id="run-switch", component_property="value")
+)
+def single_or_all_run_display(run_type):
+    if run_type == ["single"]:
+        return {"display": "block"}, {"display": "none"}
+    else:
+        return {"display": "none"}, {"display": "block"}
+
+
+
 
 # Run app
 if __name__=='__main__':
-    app.run_server(port=8052)
+    app.run_server(port=8052, debug=True)
